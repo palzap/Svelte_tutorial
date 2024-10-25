737e8a4b6e17da08c2f9e2b89c940baf210cb19b
palzap <palcleal@gmail.com>
2024-10-25 16:38:11 +0100

    conditionals and styling

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,21 @@
 <script>
+	import Modal from './Modal.svelte';
 	export let name;
 	let first_name = 'John';
 	let last_name = 'Doe';
 	let color = 'black';
+	let items = [
+		{ id: 1, name: 'Item 1', color: 'red' },
+		{ id: 2, name: 'Item 2', color: 'green' },
+		{ id: 3, name: 'Item 3', color: 'blue' },
+		{ id: 4, name: 'Item 4', color: 'yellow' },
+		{ id: 5, name: 'Item 5', color: 'purple' },
+		{ id: 6, name: 'Item 6', color: 'orange' },
+		{ id: 7, name: 'Item 7', color: 'pink' },
+		{ id: 8, name: 'Item 8', color: 'brown' },
+		{ id: 9, name: 'Item 9', color: 'gray' },
+		{ id: 10, name: 'Item 10', color: 'black' }
+	]
 
 	$: fullName = `${first_name} ${last_name}`;
 	$: {
@@ -12,7 +25,7 @@
 	}
 
 	const handleClick= () => {
-		name = 'Another World!';
+		name = 'Another World';
 	}
 
 	const handleInput = (event) => {
@@ -26,23 +39,50 @@
 		}
 		color = 'blue';
 	}
+
+	const deleteItem = (id) => {
+		items = items.filter(item => item.id != id);
+	}
 </script>
 
+
 <main>
+	<Modal />
 	<h1>Hello {name}!</h1>
 	
-	<p style="color :{color}">My name is {fullName}.</p>
-	
-	<button on:click={handleClick}>Change World!</button>
-	<button on:click={handleColor}>Change Color!</button>
+	<div>
+		<h4>
+			<p style="color :{color}">My name is {fullName}.</p>
+			
+			<label for="first_name">First Name</label>
+			<input type="text" id="first_name" bind:value={first_name}>
+			<label for="last_name">Last Name</label>
+			<input type="text" id="last_name" bind:value={last_name}>
 
-	<input type="text" bind:value={first_name}>
+			<label for="world">World name (with input handler)</label>
+			<input type="text" on:input={handleInput} value={name}>	
+			<label for="world">World name (with binding)</label>
+			<input type="text" bind:value={name}>
+		</h4>
+		<h5>
+			<button on:click={handleClick}>Change World!</button>
+			<button on:click={handleColor}>Change Color!</button>
+		</h5>
 
-	<input type="text" bind:value={last_name}>
-	
-	<input type="text" on:input={handleInput} value={name}>
-	
-	<input type="text" bind:value={name}>
+		{#each items as item (item.id)}
+			{#if item.id % 2 == 0}
+				<p>Even</p>
+			{:else}
+				<p>Odd</p>
+			{/if}
+			<div>
+				<p style="color: {item.color}">{item.name}</p>
+				<button on:click={() => deleteItem(item.id)}>delete</button>
+			</div>				
+		{:else}
+			<p>No items found!</p>
+		{/each}
+	</div>
 </main>
 
 <style>
---
+++ src/Modal.svelte
@@ -0,0 +1,34 @@
+<script>
+    let showModal = true;
+    let isPromo = true;
+</script>
+
+<div class="backdrop" class:promo={isPromo}>
+    <div class="modal">
+        <p>This is a placeholder!</p>
+    </div>
+
+</div>
+
+<style>
+    .backdrop {
+        width: 100%;
+        height: 100%;
+        position: fixed;
+        background: rgba(0, 0, 0, 0.5);
+    }
+
+    .modal {
+        padding: 10px;
+        border-radius: 10px;
+        max-width: 400px;
+        margin: 10% auto;
+        text-align: center;
+        background: white;
+    }
+
+    .promo .modal {
+        background: #ff3e00;
+        color: white;
+    }
+</style>

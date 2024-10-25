c6fce8f41e64a44acd671b147856e9a6ade2440d
palzap <palcleal@gmail.com>
2024-10-25 15:28:06 +0100

    reactive values and functions

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,48 @@
 <script>
 	export let name;
+	let first_name = 'John';
+	let last_name = 'Doe';
+	let color = 'black';
+
+	$: fullName = `${first_name} ${last_name}`;
+	$: {
+		console.log('First Name:', first_name);
+		console.log('Last Name:', last_name);
+		console.log('Full Name:', fullName);
+	}
+
+	const handleClick= () => {
+		name = 'Another World!';
+	}
+
+	const handleInput = (event) => {
+		name = event.target.value;
+	}
+
+	const handleColor = () => {
+		if (color === 'blue') {
+			color = 'black';
+			return;
+		}
+		color = 'blue';
+	}
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	
+	<p style="color :{color}">My name is {fullName}.</p>
+	
+	<button on:click={handleClick}>Change World!</button>
+	<button on:click={handleColor}>Change Color!</button>
+
+	<input type="text" bind:value={first_name}>
+
+	<input type="text" bind:value={last_name}>
+	
+	<input type="text" on:input={handleInput} value={name}>
+	
+	<input type="text" bind:value={name}>
 </main>
 
 <style>

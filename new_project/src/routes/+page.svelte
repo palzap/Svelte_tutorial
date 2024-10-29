7467504fe8a819a4d860a015450cc721e2d060c3
palzap <palcleal@gmail.com>
2024-10-29 15:36:17 +0000

    starting snippets

--- new_project/src/routes/+page.svelte
+++ new_project/src/routes/+page.svelte
@@ -1,25 +1,60 @@
 <script lang="ts">
     import Header from "./Header.svelte";
-    let name = $state("Pedro");
-    let last_name = $state("Leal");
-    let status: 'OPEN' | 'CLOSED' = $state('OPEN');
+    let formState = $state({
+        name: "",
+        birthday: "",
+        step: 0,
+        error: ""
+    });
 
-    let full_name =$derived(name + " " + last_name);
-
-    function toggle() {
-        status = status === 'OPEN' ? 'CLOSED' : 'OPEN';
+    function nextStep() {
+        if ((formState.name !== "" && formState.step === 0) || (formState.birthday !== "" && formState.step === 1)) {
+            formState.step++;
+            formState.error = "";
+        } else {
+            if (formState.step === 0) formState.error = "Name is required";
+            if (formState.step === 1) formState.error = "Birthday is required";
+        }
+        
     }
 </script>
 
-<Header {name} {last_name}/>
 
-<h2>{full_name}</h2>
+<main>
+    <Header name={formState.name} />
+    <p>Step: {formState.step + 1}</p>
 
-<input type="test" bind:value={name}/>
 
-<p>Website {status}</p>
-<button onclick={toggle}>function status</button>
-<button onclick={() => {
-    status = status === 'OPEN' ? 'CLOSED' : 'OPEN';
-    }}>inline status
-</button>
+    {#if formState.step === 0}
+        <div>
+            <label for='name'>Name</label>
+            <input type="text" id="name" bind:value={formState.name}/>
+        </div>
+        <button onclick={nextStep}>Next</button>
+    {:else if formState.step === 1}
+        <div>
+            <label for='bday'>Birthday</label>
+            <input type="date" id="bday" bind:value={formState.birthday}/>
+        </div>
+        <button onclick={nextStep}>Next</button>
+    {/if}
+
+    {#if formState.error}
+    <p class="error">{formState.error}</p>
+{/if}
+</main>
+
+<!-- {#snippet formStep({ id, question, type }; { id: string, question: string, type: string })}
+    <article>
+        <div>
+            <label for={id}>{question}</label>
+            <input {type} {id} bind:value={formState[id]}>
+        </div>
+    </article>    
+{/snippet} -->
+
+<style>
+    .error {
+        color: red;
+    }
+</style>
--- new_project/src/routes/Header.svelte
+++ new_project/src/routes/Header.svelte
@@ -1,15 +1,14 @@
 <script lang="ts">
     let { 
-        name, 
-        last_name
+        name
     }: {
         name: string;
-        last_name?: string;
     } = $props()
 </script>
 
-<h1>Hello {name} {last_name}</h1>
-<h2>{name.replaceAll('e', 'x')}</h2>
+<div>
+    <h1>{name ? name : 'User'} registering</h1>
+</div>
 
 <style>
 
